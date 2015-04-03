297f18f52d61d77f32b2e99ec1442415b9a945b0
truNEET <gtorrent.gentoo@gmail.com>
2015-04-03 11:18:09 -0500

    Add base kari core file

---
+++ include/kari/core.hpp
@@ -0,0 +1,18 @@
+#ifndef KARI_HPP
+#define KARI_HPP
+
+#include <kari/rectangle.hpp>
+#include <string>
+
+namespace ka {
+
+class Core
+{
+public:
+	Core();
+	void sendRegion(const ka::Rectangle &region, const std::string &service);
+};
+
+}
+
+#endif
---
+++ src/core.cpp
@@ -0,0 +1,13 @@
+#include <kari/core.hpp>
+
+namespace ka {
+
+Core::Core()
+{
+}
+
+void Core::sendRegion(const ka::Rectangle &region, const std::string &service)
+{
+}
+
+}
