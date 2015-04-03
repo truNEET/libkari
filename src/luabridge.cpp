96d8cf91ea7b15d4d4fef4437ee6e14b81f0b556
truNEET <gtorrent.gentoo@gmail.com>
2015-04-03 16:18:00 -0500

    Remove old luabridge and service file

--- src/luabridge.cpp
+++
@@ -1,9 +0,0 @@
-#include <kari/luabridge.hpp>
-
-namespace ka {
-
-LuaBridge::LuaBridge(const std::string &file)
-{
-}
-
-}
--- src/service.cpp
+++
@@ -1,9 +0,0 @@
-#include <kari/service.hpp>
-
-namespace ka {
-
-Service::Service(const std::string &service)
-{
-}
-
-}
