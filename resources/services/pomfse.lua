6e282999a797a9621e5bc0dabf528a4cbfd1fba3
truNEET <gtorrent.gentoo@gmail.com>
2015-04-03 16:15:55 -0500

    Start example pomf.se bridge

--- resources/services/pomfse.lua
+++ resources/services/pomfse.lua
@@ -3,5 +3,6 @@
 ----------------------------------------------------------
 
 function init()
-	register_service( { name = "POMF.SE", author = "truNEET"  } )
+	--register_service( { name = "POMF.SE", author = "truNEET"  } )
+	print('Hello, World!')
 end
