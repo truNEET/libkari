c28a64f1600cac3c34a1afa97fc313e3a0babcb7
truNEET <gtorrent.gentoo@gmail.com>
2015-04-03 17:20:50 -0500

    Modify test service

--- resources/services/pomfse.lua
+++ resources/services/pomfse.lua
@@ -2,7 +2,9 @@
 		  -- Pomf.se Manager Service for Kari --
 ----------------------------------------------------------
 
-function init()
+register_service()
+
+--function init()
 	--register_service( { name = "POMF.SE", author = "truNEET"  } )
-	print('Hello, World!')
-end
+	--print('Hello, World!')
+--end
