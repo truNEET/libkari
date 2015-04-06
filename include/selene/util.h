027792ca146e09626df3b0ea733ba5c4e61c22e6
truNEET <gtorrent.gentoo@gmail.com>
2015-04-06 15:33:35 -0500

    Modify selene header to reflect local project's lua include location

--- include/selene/util.h
+++ include/selene/util.h
@@ -3,9 +3,9 @@
 #include <iostream>
 
 extern "C" {
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
+#include <lua/lua.h>
+#include <lua/lauxlib.h>
+#include <lua/lualib.h>
 }
 
 namespace sel {
